de965a6035fd061741d532e3713bc1a5bcb98240
Chris Owen <c.ornine@gmail.com>
2024-06-01 14:40:33 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 
-interface GraphNode {
+interface NodePoint {
 		input: number,
 		value: number,
 }
 
-function getEnvelopeEdge(startNode: GraphNode, endNode:GraphNode): (input: number) => number {
-	const inputRange = endNode.input - startNode.input;
-	const valueRange = endNode.value - startNode.value;
+function getEdge(start: NodePoint, end:NodePoint): (input: number) => number {
+	const inputRange = end.input - start.input;
+	const valueRange = end.value - start.value;
 
 	return (input:number):number => {
-		const percentage = (input - startNode.input) / inputRange;
-		return startNode.value + valueRange * percentage;
+		return start.value + valueRange * ((input - start.input) / inputRange);
 	};
 }
 
 
-function createEnvelope(nodeArray: GraphNode[]): (input: number) => number {
-	const nodeBreakPoints = nodeArray.map((node) => node.input);
-	const lookup = [(input: number) => nodeArray[0].value];
-	for (let i = 0; i < nodeArray.length - 1; i++) {
-		lookup.push(getEnvelopeEdge(nodeArray[i], nodeArray[i + 1]));
+function createEnvelope(nodePointArray: NodePoint[]): (input: number) => number {
+	const nodeBreakPoints = nodePointArray.map((node) => node.input);
+	const lookup = [(input: number) => nodePointArray[0].value];
+	
+	for (let i = 0; i < nodePointArray.length - 1; i++) {
+		lookup.push(getEdge(nodePointArray[i], nodePointArray[i + 1]));
 	}
 
 	return (input: number): number => {
-		let output = nodeArray[nodeArray.length - 1].value;
+		let output = nodePointArray[nodePointArray.length - 1].value;
 
 		for (let i = 0; i < nodeBreakPoints.length; i++) {
 			if (input <= nodeBreakPoints[i]) {
